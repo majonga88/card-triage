82b7ae81c4bef8ee728d3b3e02b199a4b797f351
majonga88 <majonga.agnojam@gmail.com>
2023-06-05 12:34:41 +0200

    add index + hot reload + scripts + mode + other configurations

---
+++ app/src/index.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+
+const App: React.FC = () => {
+  return (
+      <div>
+        <h1>Hello, React! aa</h1>
+      </div>
+  );
+};
+
+ReactDOM.render(<App />, document.getElementById('root'));
---
+++ app/webpack.config.js
@@ -0,0 +1,45 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  mode: 'development',
+  entry: './src/index.tsx',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(ts|js)x?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+    }),
+  ],
+  devServer: {
+    static: {
+      directory: path.resolve(__dirname, 'dist'),
+    },
+    compress: true,
+    hot: true,
+    port: 9000,
+  },
+  performance: {
+    maxAssetSize: 500000,
+  },
+};
